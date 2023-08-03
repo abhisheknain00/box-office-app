43e844c3b9e19cfd877ae21363a23c38d992f52c
Pratiksha Kumari <pratikshakumari@Pratikshas-MacBook-Air.local>
2023-08-04 02:21:38 +0530

    installed react-query. replaced useShowDataById with useQuery hook

--- src/components/actors/ActorsCard.jsx
+++ src/components/actors/ActorsCard.jsx
@@ -1,5 +1,3 @@
-import { Link } from 'react-router-dom';
-
 const ActorsCard = ({ name, image, gender, country, birthday, deathday }) => {
   return (
     <div>
--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,31 +1,14 @@
+import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
-import { useEffect, useState } from 'react';
 import { getShowById } from '../api/tvmaze';
-
-const useShowDataById = showId => {
-  const [showData, setShowData] = useState(null);
-  const [showError, setShowError] = useState(null);
-
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const data = await getShowById(showId);
-        setShowData(data);
-      } catch (err) {
-        setShowError(err);
-      }
-    }
-
-    fetchData();
-  }, [showId]);
-
-  return { showData, showError };
-};
 
 const Show = () => {
   const { showId } = useParams();
 
-  const { showData, showError } = useShowDataById(showId);
+  const { data: showData, error: showError } = useQuery({
+    queryKey: ['show', showId],
+    queryFn: () => getShowById(showId),
+  });
 
   if (showError) {
     return <div>We have an Error: {showError.message}</div>;
